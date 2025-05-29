840c15ad99939e42c6e8f5360fd04735b7321d7b
Todd Wallace <todd@tektodd.com>
2025-05-29 15:18:37 -0400

    Updated linux build script to use c++17 standard now

--- dragonwx_linux_build.sh
+++ dragonwx_linux_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -n "$1" ] && [ -n "$2" ]; then
-	g++ -I"$1" -I"$1"/include -o "$2" Main.cpp DragonWx.cpp -lfreetype -lX11 -lGL -lpthread -lpng -lstdc++fs -lcurl -std=c++20
+	g++ -I"$1" -I"$1"/include -o "$2" Main.cpp DragonWx.cpp -lfreetype -lX11 -lGL -lpthread -lpng -lstdc++fs -lcurl -std=c++17
 else
 	echo Syntax: "$0" "<Project directory path> <Executable output filename>"
 fi
